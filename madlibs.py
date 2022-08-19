7df6c0c8ff1549ca742c5c86a5fe8e8c91d91792
mahmud10cool <mutashi@gmail.com>
2022-08-19 12:45:38 +0600

    Just for funsies

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+# String concatenation 
+# Suppose we want to create a string that says "subscribe to ______"
+youtuber = "" 
+
+print("Subscribe to " + youtuber)
+print("Subscribe to {}".format(youtuber))
+print(f"Subscribe to {youtuber}")
