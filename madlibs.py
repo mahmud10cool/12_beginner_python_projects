a29d376a5106ae070ae4c3d9c56615592ab9440d
mahmud10cool <mutashi@gmail.com>
2022-08-19 12:57:42 +0600

    Done with project 1

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,17 @@
 # String concatenation 
 # Suppose we want to create a string that says "subscribe to ______"
-youtuber = "" 
+# youtuber = "" 
 
-print("Subscribe to " + youtuber)
-print("Subscribe to {}".format(youtuber))
-print(f"Subscribe to {youtuber}")
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format(youtuber))
+# print(f"Subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
