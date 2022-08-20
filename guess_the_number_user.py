789b9a4bfc240343f5aaa6b1cebf242a74bfc1ab
mahmud10cool <mutashi@gmail.com>
2022-08-20 13:33:10 +0600

    User guess the number

---
+++ guess_the_number_user.py
@@ -0,0 +1,24 @@
+import random 
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high because low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
+def main():
+    computer_guess(1000)
+
+if __name__ == '__main__':
+    main()
